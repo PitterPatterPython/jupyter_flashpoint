949b1dc985fa59f3946a4989d5f012c8f67bc656
Rob D'Aveta <rob.daveta@gmail.com>
2023-11-03 23:04:17 -0400

    first publish of integration

--- flashpoint_core/flashpoint_full.py
+++ flashpoint_core/flashpoint_full.py
@@ -2,14 +2,16 @@
 
 import pandas as pd
 from integration_core import Integration
+from IPython.core.display import HTML
 from IPython.core.magic import (magics_class, line_cell_magic)
+from IPython.display import display
 from flashpoint_core._version import __desc__
-
 
 import jupyter_integrations_utility as jiu
 from utils.flashpoint_api import FlashpointAPI
 from utils.user_input_parser import UserInputParser
-from jupyter_flashpoint.utils.api_response_parser import ResponseParser
+from utils.api_response_parser import ResponseParser
+from utils.helper_functions import format_b64_image_for_dataframe
 
 
 @magics_class
@@ -102,6 +104,32 @@ class Flashpoint(Integration):
         return out
     
     def customQuery(self, query : str, instance : str):
+        """ Execute a custom cell magic against the Flashpoint API.
+            
+            START HERE -- General flow of this function:
+            1.  We need to parse the user's cell magic via ../utils/user_input_parser. We
+                construct an object there that has metadata. We use that object to drive
+                the rest of this function.
+            2.  We'll display errors if there were any obvious ones during parsing.
+            3.  Using the parsed input's "input" object, we'll send those to the Flashpoint
+                API's _handler function via ../utils/flashpoint_api. The _handler function
+                plays traffic cop for every API call. 
+            4.  I follow this simple idiom: the API is there to retrieve data, not format it.
+                That's why I delegate parsing the API response to ../utils/api_response_parser.
+                Just like the API, we pass the response to the _handler function of our response
+                parser, and it takes care of the rest. It's basically magic.
+            5.  We choose how to handle the parsed data based on the type of data we're
+                handling. That's what those if/elif/else statements are for.
+            
+            Keyword Arguments:
+            query -- this is what the user types in the cell
+            instance -- this is the instance/connection we'll run the query against
+
+            Returns:
+            dataframe -- a pandas dataframe, or None
+            status -- sent back to jupyter_integration_base
+        """
+        
         # Parse the supplied user input via the cell magic using
         # the user_input_parser utility in ../utils
         parsed_input = self.user_input_parser.parse_input(query)
@@ -112,28 +140,39 @@ class Flashpoint(Integration):
 
         if parsed_input["error"] == True:
             jiu.displayMD(f"**[ ! ]** {parsed_input['message']}")
-            mydf = None
-            status = parsed_input["message"]
+            dataframe = None
+            status = f"Failure: {parsed_input['message']}"
 
         else:
             try:
                 # Execute the query to the Flashpoint API by sending the user's parsed input
-                response = self.instances[instance]["session"].handler(**parsed_input["input"])
+                response = self.instances[instance]["session"]._handler(**parsed_input["input"])
                 
                 # Pass the response to the response parser, which is responsible for 
                 # transforming API responses from Flashpoint into a structure that can 
                 # move into a dataframe
-                parsed_response = self.response_parser.handler(parsed_input["input"]["command"], response.json())
+                parsed_response = self.response_parser._handler(parsed_input["input"]["command"], response)
 
-                mydf = pd.DataFrame(parsed_response)
-                status = "Success"
+                if parsed_input["input"]["command"] == "get_image":
+                    display(HTML(parsed_response))
+                    dataframe = None
+                    status = "Success - No Results"
+
+                elif (parsed_input["input"]["command"] == "search_media") and (parsed_input["input"]["images"]):
+                    dataframe = pd.DataFrame(parsed_response)
+                    display(HTML(dataframe.to_html(formatters={"image_content": format_b64_image_for_dataframe}, escape=False)))
+                    status = "Success"
+                
+                else:
+                    dataframe = pd.DataFrame(parsed_response)
+                    status = "Success"
             
             except Exception as e:
                 jiu.displayMD(f"**[ ! ]** Error during execution: {e}")
-                mydf = None
-                status = str(e)
+                dataframe = None
+                status = f"Failure - {e}"
 
-        return mydf, status
+        return dataframe, status
 
     # This is the magic name.
     @line_cell_magic
