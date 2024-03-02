0db1021d0fb8c02997975e63f707e71f2e4dbcc0
Rob D'Aveta <rob.daveta@gmail.com>
2024-03-01 19:15:57 -0500

    linted, added OCR text to output

--- flashpoint_utils/api_response_parser.py
+++ flashpoint_utils/api_response_parser.py
@@ -1,6 +1,7 @@
 import jsonpath_ng as jp
 from flashpoint_utils.helper_functions import create_b64_image_string, format_b64_image_for_dataframe
 
+
 class ResponseParser:
     """
     A class to parse API responses from the Flashpoint API.
@@ -30,21 +31,21 @@ class ResponseParser:
         """ Brokers response parsing on behalf of the calling function
 
             Keyword arguments:
-            issued_command -- the command that the user executed, which 
+            issued_command -- the command that the user executed, which
                 represents a function name below (isn't that cool?)
             response -- the requests.Response object from the API call
 
-            Returns: 
+            Returns:
             Whatever is passed back to it. It's a broker. It don't care.
-            
+
         """
-        return getattr(self,issued_command)(response)
-    
+        return getattr(self, issued_command)(response)
+
     def _find_value_by_path(self, path: str, json: dict):
         """ Look up a value of a JSON object via JSONPath (jsonpath_ng)
-            Note: just going to leave this here for reference, if your JSON legitimately 
+            Note: just going to leave this here for reference, if your JSON legitimately
                 has special characters in it, like '/': https://stackoverflow.com/questions/62006612/jsonpath-ng-lexer-jsonpathlexererror-error-on-line-1-col-8-unexpected-charact
-            
+
             Keyword arguments:
             path -- the JSON path to find in the JSON
             json -- the JSON to search through
@@ -54,7 +55,6 @@ class ResponseParser:
         """
         query = jp.parse(path)
         return (", ").join(match.value for match in query.find(json))
-            
 
     def search_media(self, response):
         """ Flatten a media search API response from the Flashpoint API
@@ -62,7 +62,7 @@ class ResponseParser:
 
             Keyword arguments:
             response -- the requests.Response object from the Flashpoint API call
-            
+
             Returns:
             flattened_response -- a flattened list of dictionaries to be
                 used in a dataframe
@@ -87,26 +87,26 @@ class ResponseParser:
             "_source.container.fpid",
             "_source.media.storage_uri",
             "_source.site_actor.names.aliases[*]",
-            "_source.site_actor.native_id"
+            "_source.site_actor.native_id",
+            "_source.media_v2[0].image_enrichment.enrichments.v1.image-analysis.text[0].value"
         ]
 
         for hit in hits:
             row_to_add = {}
             for path in paths_to_extract:
-                row_to_add.update({ path : self._find_value_by_path(path, hit) })
+                row_to_add.update({path: self._find_value_by_path(path, hit)})
             flattened_response.append(row_to_add)
 
         return flattened_response
 
     def get_image(self, response):
         """ Convert a raw image bytes object to a hot-n-ready $5 HTML-ready object
-        
+
             Keyword arguments:
             response -- the requests.Response object from the Flashpoint API call
 
             Returns:
-            An HTML-ready b64 string of the image 
+            An HTML-ready b64 string of the image
         """
         b64_img_data = create_b64_image_string(response.content)
         return format_b64_image_for_dataframe(b64_img_data)
-
